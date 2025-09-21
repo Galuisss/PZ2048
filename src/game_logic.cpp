a1653bc548377ee98f1ad83769383b0ab5ec49aa
pxy <controlvector@sjtu.edu.cn>
2025-09-21 17:58:03 +0800

    implement most of game_logic.cpp except PrintBoard

--- src/game_logic.cpp
+++ src/game_logic.cpp
@@ -19,58 +19,241 @@ namespace PZ2048 {
 // After all, the game logic on the online judger won't contain extern variables same as yours.
 // You are free to add helper functions.
 
+int row_num_ = 4, col_num_ = 4;
+int target_ = 0;
+uint game_seed_ = 0;
+bool undone_ = true;
+int steps_ = 0;
+
+int board_[10][10];
+int score_ = 0;
+int largest_ = 2;
+
+int board_last_[10][10];
+int board_temp_[10][10];
+int score_last_ = 0;
+int largest_last_ = 2;
+
 void Start(int row_num, int col_num, int target, uint game_seed) {
-    /** implement here. **/
+    row_num_ = row_num, col_num_ = col_num;
+    target_ = target;
+    game_seed_ = game_seed;
+    undone_ = true;
+
+    for (int i = 0; i < row_num; i++)
+        for (int j = 0; j < col_num; j++) {
+            board_[i][j] = 0;
+        }
+
+    steps_ = 0;
+
+    score_ = 0;
+    largest_ = 2;
+
+    srand(game_seed);
+    TryGenerateTile();
 }
 
 std::pair<int, int> EndGame() {
-    /** implementation here **/
+    return std::make_pair(steps_, score_);
 }
 
 int GetRows() {
-    /** implementation here **/
+    return row_num_;
 }
 
 int GetCols() { 
-    /** implementation here **/
+    return col_num_;
 }
 
 bool TryRun(char dir) {
-    /** implementation here **/
+    int score_temp_ = score_;
+    int largest_temp_ = largest_;
+    for (int i = 0; i < row_num_; i++)
+        for (int j = 0; j < col_num_; j++) {
+            board_temp_[i][j] = board_[i][j];
+        }
+
+    switch (dir)
+    {
+    case 'w':
+        for (int j = 0; j < col_num_; j++) {
+            int pos = 0, value = 0, now;
+            for (int i = 0; i < row_num_; i++) {
+                now = board_[i][j];
+                if (now == 0) continue;
+                board_[i][j] = 0;
+                if (value == 0) value = board_[pos][j] = now;
+                else if (value == now) {
+                        value = board_[pos][j] *= 2;
+                        score_ += value;
+                        if (value > largest_) {
+                            largest_ = value;
+                        }
+                        value = 0;
+                        ++pos;
+                    }
+                else {
+                        ++pos;
+                        value = board_[pos][j] = now;
+                }
+            }
+        }
+        break;
+
+    case 'a':
+        for (int i = 0; i < row_num_; ++i) {
+            int pos = 0, value = 0, now;
+            for (int j = 0; j < col_num_; ++j) {
+                now = board_[i][j];
+                if (now == 0) continue;
+                board_[i][j] = 0;
+                if (value == 0) {
+                    value = board_[i][pos] = now;
+                } else if (value == now) {
+                    value = board_[i][pos] *= 2;
+                    score_ += value;
+                    if (value > largest_) {
+                        largest_ = value;
+                    }
+                    value = 0;
+                    ++pos;
+                } else {
+                    ++pos;
+                    value = board_[i][pos] = now;
+                }
+            }
+        }
+        break;
+
+    case 's':
+        for (int j = 0; j < col_num_; ++j) {
+            int pos = row_num_ - 1;
+            int value = 0;
+            for (int i = row_num_ - 1; i >= 0; --i) {
+                int now = board_[i][j];
+                if (now == 0) continue;
+                board_[i][j] = 0;
+                if (value == 0) {
+                    value = board_[pos][j] = now;
+                } else if (value == now) {
+                    value = board_[pos][j] *= 2;
+                    score_ += value;
+                    if (value > largest_) {
+                        largest_ = value;
+                    }
+                    value = 0;
+                    --pos;
+                } else {
+                    --pos;
+                    value = board_[pos][j] = now;
+                }
+            }
+        }
+        break;
+
+    case 'd':
+        for (int i = 0; i < row_num_; ++i) {
+            int pos = col_num_ - 1;
+            int value = 0;
+            for (int j = col_num_ - 1; j >= 0; --j) {
+                int now = board_[i][j];
+                if (now == 0) continue;
+                board_[i][j] = 0;
+                if (value == 0) {
+                    value = board_[i][pos] = now;
+                } else if (value == now) {
+                    value = board_[i][pos] *= 2;
+                    score_ += value;
+                    if (value > largest_) {
+                        largest_ = value;
+                    }
+                    value = 0;
+                    --pos;
+                } else {
+                    --pos;
+                    value = board_[i][pos] = now;
+                }
+            }
+        }
+        break;
+
+    default:
+        return false;
+    }
+
+    bool has_changed = false;
+    for (int i = 0; i < row_num_; i++) {
+        if (has_changed) break;
+        for (int j = 0; j < col_num_; j++) {
+            if (has_changed) break;
+            has_changed |= board_[i][j] != board_temp_[i][j];
+        }
+    }
+
+    if (!has_changed) return false;
+
+    for (int i = 0; i < row_num_; i++)
+        for (int j = 0; j < col_num_; j++) {
+            board_last_[i][j] = board_temp_[i][j];
+        }
+    undone_ = false;
+    score_last_ = score_temp_;
+    largest_last_ = largest_temp_;
+    ++steps_;
+    TryGenerateTile();
+    return true;
 }
 
 bool Undo() {
-    /** implement here. **/
-    /**in the process you need to undo the operation and both the score**/
-    /**for example we have 0 0 0 4 4pts from 2 0 0 2 0pts, after we undo, we get 2 0 0 2 0pts**/
+    if (undone_) return false;
+
+    undone_ = true;
+    for (int i = 0; i < row_num_; i++)
+        for (int j = 0; j < col_num_; j++) {
+            board_[i][j] = board_last_[i][j];
+        }
+
+    score_ = score_last_;
+    largest_ = largest_last_;
+    --steps_;
+    return true;
 }
 
 void SetTile(int row_index, int col_index, int value) {
-    /** implement here. **/
+    board_[row_index][col_index] = value;
+    if (value > largest_) {
+        largest_ = value;
+    } 
 }
 
 int GetTile(int row_index, int col_index) {
-    /** implement here. **/
+    return board_[row_index][col_index];
 }
 
 int Score() { 
-    /** implement here. **/ 
+    return score_;
 }
 
 int Steps() { 
-    /** implement here. **/ 
+    return steps_;
 }
 
 int GetTarget() { 
-    /** implement here. **/ 
+    return target_;
 }
 
 bool HasReachedTarget() {
-    /** implement here. **/
+    return largest_ >= target_;
 }
 
 bool Stuck() {
-    /** implement here. **/
+    for (int i = 0; i < row_num_; i++)
+        for (int j = 0; j < col_num_; j++) {
+            if (board_[i][j] == 0) return false;
+            if (i < row_num_ - 1 && j < col_num_ - 1 && (board_[i][j] == board_[i][j+1] || board_[i][j] == board_[i+1][j])) return false;
+        }
+    return true;
 }
 
 void PrintBoard() {
